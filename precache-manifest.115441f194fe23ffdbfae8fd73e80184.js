e3c56200616cdb23ca8a3a58686c88d0a4e7ca7f
nelsonlpco <nelson_pc6@hotmail.com>
2019-03-12 20:09:59 -0300

    Updates

---
+++ precache-manifest.115441f194fe23ffdbfae8fd73e80184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd5630d97d922c6c8f84",
+    "url": "/code-hero/static/css/main.cb08d95f.chunk.css"
+  },
+  {
+    "revision": "bd5630d97d922c6c8f84",
+    "url": "/code-hero/static/js/main.5d2bc7aa.chunk.js"
+  },
+  {
+    "revision": "6561509a09ef762987df",
+    "url": "/code-hero/static/js/runtime~main.79f0f47e.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/code-hero/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/code-hero/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a04b2a3d24b09619fac0ad4a09e88ce9",
+    "url": "/code-hero/index.html"
+  }
+];
